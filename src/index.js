737d6214dfbf5c5a490b2bca199aaba034e54e15
Alexander <122707746+euricalex@users.noreply.github.com>
2023-07-11 15:02:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,50 +3,52 @@ import { fetchBreeds } from "./cat-api.js";
 import { fetchCatByBreed } from "./cat-api.js";
 
 
-
-const selectList = document.querySelector('select.breed-select');
-const catInfo = document.querySelector('div.cat-info');
-const choices = new Choices(selectList, {
- searchEnabled: false,
-  itemSelectText: ""
-});
-fetchBreeds().then(breeds => {
-  // Створити варіанти для вибору порід котів
-  const options = breeds.map(breed => ({
-    value: breed.value,
-    label: breed.label
-  }));
-
-  // Додати перший елемент до списку варіантів
-  options.unshift({
-    value: '',
-    label: 'Select a breed'
+document.addEventListener('DOMContentLoaded', () => {
+  const selectList = document.querySelector('select.breed-select');
+  const catInfo = document.querySelector('div.cat-info');
+  const choices = new Choices(selectList, {
+   searchEnabled: false,
+    itemSelectText: ""
   });
-
-  // Оновити список варіантів у селекті
-  choices.setChoices(options, 'value', 'label');
-  choices.setChoiceByValue('');
-});
-
-    
-    selectList.addEventListener('change', () => {
-const selectBreedID = selectList.value;
-fetchCatByBreed(selectBreedID)
-    .then(catData => {
-      const imageCat = document.createElement('img');
-      imageCat.classList.add("image-cat");
-      imageCat.src = catData.url;
-
-      catInfo.innerHTML = `
-      <div class="cat-details">
-      <p class="name"><strong>${catData.breeds[0].name}</strong> </p>
-      <p class="desc"> ${catData.breeds[0].description}</p>
-      <p class="temp"><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
-    </div>
-`;
-    catInfo.prepend(imageCat);
+  fetchBreeds().then(breeds => {
+    // Створити варіанти для вибору порід котів
+    const options = breeds.map(breed => ({
+      value: breed.value,
+      label: breed.label
+    }));
+  
+    // Додати перший елемент до списку варіантів
+    options.unshift({
+      value: '',
+      label: 'Select a breed'
     });
-    })
+  
+    // Оновити список варіантів у селекті
+    choices.setChoices(options, 'value', 'label');
+    choices.setChoiceByValue('');
+  });
+  
+      
+      selectList.addEventListener('change', () => {
+  const selectBreedID = selectList.value;
+  fetchCatByBreed(selectBreedID)
+      .then(catData => {
+        const imageCat = document.createElement('img');
+        imageCat.classList.add("image-cat");
+        imageCat.src = catData.url;
+  
+        catInfo.innerHTML = `
+        <div class="cat-details">
+        <p class="name"><strong>${catData.breeds[0].name}</strong> </p>
+        <p class="desc"> ${catData.breeds[0].description}</p>
+        <p class="temp"><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
+      </div>
+  `;
+      catInfo.prepend(imageCat);
+      });
+      });
+})
+
 
     
 
